64f2d8b0ced112f97b5eafe4a79999c74bad46bb
Adarsh321123 <adarshk1234567@gmail.com>
2024-05-17 15:27:47 -0700

    add theorems

--- Lean4TraceTest/Basic.lean
+++ Lean4TraceTest/Basic.lean
@@ -1,1 +1,13 @@
-def hello := "world"
+open Nat (add_assoc add_comm)
+
+theorem hello_world (a b c : Nat) : a + b + c = a + c + b := by
+  sorry
+
+theorem foo (a : Nat) : a + 1 = Nat.succ a := by
+  sorry
+
+theorem bar (a b : Nat) : a + b = b + a := by
+  rw[add_comm]
+
+theorem baz (a b c d : Nat) : a + b + c + d = a + c + d + b := by
+  sorry
